bf876a36309fa18db2c6d17d134e989e1aad4afa
LarryYu <linuxprobe.org@gmail.com>
2019-06-17 23:21:01 +0800

    更新获取用户ip地址

--- src/main/java/org/linuxprobe/luava/http/HttpServletUtils.java
+++ src/main/java/org/linuxprobe/luava/http/HttpServletUtils.java
@@ -109,7 +109,7 @@ public class HttpServletUtils {
 			}
 		}
 		/** 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割 */
-		if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length() = 15
+		if (StringUtils.isNoneBlank(ipAddress)) {
 			if (ipAddress.indexOf(",") > 0) {
 				ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
 			}
